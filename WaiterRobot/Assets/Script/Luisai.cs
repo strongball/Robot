2f1d9ff4565498505c2be15d29e5b8b45a1d4b8f
hung0123 <flyking1486325@gmail.com>
2016-07-14 10:26:58 +0800

    1

--- WaiterRobot/Assets/Script/Luisai.cs
+++ WaiterRobot/Assets/Script/Luisai.cs
@@ -7,17 +7,23 @@ using LitJson;
 
 public class Luisai : MonoBehaviour
 {
-	public UnityEvent MyEvent;
+
 	JsonData itemdata;
 	Speech speech;
 	TextToSpeech textToSpeech;
-	void Start()
+
+    IntentManager intentManager;
+    void Start()
 	{
-		if (MyEvent == null)
-			MyEvent = new UnityEvent();
+        intentManager = new IntentManager();
+
 		Speech.addListener(callLuis);
-	}
-		IEnumerator WaitForRequest(WWW www)
+        this.callLuis("一份牛排");
+
+    }
+
+
+	IEnumerator WaitForRequest(WWW www)
     {
         yield return www;
         itemdata = JsonMapper.ToObject(www.text);
@@ -25,7 +31,7 @@ public class Luisai : MonoBehaviour
     }
     public void callLuis(string input)//input is the input string
     {
-		string url = "https://api.projectoxford.ai/luis/v1/application?id=929b06ed-deda-479c-82a7-a135b681fe12&subscription-key=c02b958474214f15bbe7ffd005ef041d&q="
+		string url = "https://api.projectoxford.ai/luis/v1/application?id=020e4d3e-8694-45b0-b07d-11f2a3cc6a6f&subscription-key=c02b958474214f15bbe7ffd005ef041d&q="
         + System.Uri.EscapeDataString(input);
         WWW www = new WWW(url);
         StartCoroutine(WaitForRequest(www));
@@ -33,31 +39,18 @@ public class Luisai : MonoBehaviour
 	public void DialogIntents(JsonData itemdata)
 	{
 		string s = itemdata["intents"][0]["intent"].ToString();
-		/*if ((itemdata["intents"][0]["intent"].ToString()) == "吃飯對話")
-		{
-			s = "吃飯對話";
-		}
-		else if ((itemdata["intents"][0]["intent"].ToString()) == "日常對話")
-		{
-			s = "日常對話";
-		}
-		else if ((itemdata["intents"][0]["intent"].ToString()) == "問候")
-		{
-			s = "問候";
-		}
-		else if ((itemdata["intents"][0]["intent"].ToString()) == "垃圾話")
-		{
-			s = "垃圾話";
-		}
-		else
-		{
-			s = "你腦殘";
-		}*/
-		Toast.makeText(s, false);
-		if(s == "拍照")
-		{
-			MyEvent.Invoke();
-		}
+        List<Entity> es = new List<Entity>();
+        
+        for(int i = 0; i < itemdata["entities"].Count; i++)
+        {
+            var e = itemdata["entities"][i];
+            es.Add(new Entity(e["type"].ToString(), e["entity"].ToString()));
+        }
+        
+        intentManager.HandleIntent(new IntentEntity(s, es));
+
+        //Toast.makeText(s, false);
+
 		if (s.Length > 1)
 		{
 			TextToSpeech.Say(s);
@@ -68,4 +61,5 @@ public class Luisai : MonoBehaviour
 		}
 		
 	}
+    
 }
---
+++ WaiterRobot/Assets/Script/Response/OrdermealDialog.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class OrdermealDialog: IntentHandler
+{
+    public OrdermealDialog()
+    {
+        this.intent = Intent.Order;
+    }
+
+    public override void FitIntent(IntentEntity ie)
+    {
+        foreach (in 
+    }
+}
